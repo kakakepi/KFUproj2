7102b5eed434e2f61ccdf86c404c04727d330f9d
Семён Осипов <semenosipov@zov.local>
2024-11-13 16:58:31 +0300

    добавил summary

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("kpfu-proj")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("kpfu-proj")]
-[assembly: System.Reflection.AssemblyTitleAttribute("kpfu-proj")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc0f945160eb61ed9d1f875080843c103daed4b")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
