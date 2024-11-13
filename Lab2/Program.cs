9039052102343a6ba1d22cf92a1f792ebf87f33c
Семён Осипов <semenosipov@zov.local>
2024-11-13 17:04:57 +0300

    изменил summary

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -2,39 +2,74 @@
 
 namespace Lab_W_2{
     class Program {
+        /// <summary>
+        /// Перечисление для представления типов банковского счета.
+        /// Включает два типа: Текущий и Сберегательный.
+        /// </summary>
         enum TypeOfBankAccount {
-            /* Создаю перечислимый тип данных отображающий виды банковского
-            счета (текущий и сберегательный). */
             Текущий,
             Сберегательный
         }
+
+        /// <summary>
+        /// Структура для хранения информации о банковском счете.
+        /// Содержит номер счета, тип счета и текущий баланс.
+        /// </summary>
         struct UserBankAccInfo {
-            /*Создать структуру данных, которая хранит информацию о банковском
-            счете – его номер, тип и баланс.*/
+            /// <summary>
+            /// Номер банковского счета.
+            /// </summary>
             public int AccNumber;
+
+            /// <summary>
+            /// Тип банковского счета (Текущий или Сберегательный).
+            /// </summary>
             public TypeOfBankAccount BankType;
+
+            /// <summary>
+            /// Баланс на банковском счете.
+            /// </summary>
             public int AccBalance;
         }
+
+        /// <summary>
+        /// Перечисление для представления названия университета.
+        /// Включает ВУЗы: КГУ, КАИ, КХТИ.
+        /// </summary>
         enum UniversityName {
-            /*Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}. */
             КГУ,
             КАИ,
             КХТИ
         }
+
+        /// <summary>
+        /// Структура для хранения информации о работнике университета.
+        /// Содержит имя работника и название университета.
+        /// </summary>
         struct UnviersityWorker {
-            /* Создать структуру работник с двумя полями: имя, ВУЗ. */
+            /// <summary>
+            /// Имя работника университета.
+            /// </summary>
             public string name;
+
+            /// <summary>
+            /// Университет, в котором работает сотрудник (КГУ, КАИ или КХТИ).
+            /// </summary>
             public UniversityName university;
         }
         static void Main (string[] args) {
 
             Console.WriteLine("\nУпражнение №3.1\n");
-
+        
+            /* Создаю перечислимый тип данных отображающий виды банковского
+            счета (текущий и сберегательный). */
             TypeOfBankAccount UserAcc = TypeOfBankAccount.Сберегательный;
             Console.WriteLine($"Тип счета в банке у какого-то пользователя - {UserAcc}");
 
             Console.WriteLine("\nУпражнение №3.2\n");
 
+            /*Создать структуру данных, которая хранит информацию о банковском
+            счете – его номер, тип и баланс.*/
             UserBankAccInfo RandUserInfo = new UserBankAccInfo();
             RandUserInfo.AccBalance = 228228228;
             RandUserInfo.AccNumber = 666666;
@@ -43,6 +78,8 @@ namespace Lab_W_2{
 
             Console.WriteLine("\nУпражнение №3.3\n");
 
+            /*Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}.
+            Создать структуру работник с двумя полями: имя, ВУЗ. */
             UnviersityWorker Tutor = new UnviersityWorker();
             Tutor.name = "Иван Золо";
             Tutor.university = UniversityName.КГУ;
